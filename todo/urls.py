a58b227e96070dc60d09705549cc838955549908
Thrilesh <naiduthrilesh@gmail.com>
2023-11-13 16:25:49 +0530

    Add Task Functionality

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,8 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("add-task/", views.add_task, name="add-task"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from . models import Task
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'todo/home.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    context = {
+        'task': tasks,
+    }
+    return render(request, 'todo/home.html', context)
+
+
+def add_task(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect("home")
