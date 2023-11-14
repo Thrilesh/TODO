b1a19c975c1748a9dabf6c4cb6ef6e1fd9f22008
Thrilesh <naiduthrilesh@gmail.com>
2023-11-14 15:03:36 +0530

    TODO Final

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("add-task/", views.add_task, name="add-task"),
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark-as-done'),
+    path("mark_as_undone/<int:pk>/", views.mark_as_undone, name='mark-as-undone'),
+    path('edit_task/<int:pk>/', views.edit_task, name='edit-task'),
+    path('delete_task/<int:pk>/', views.delete_task, name='delete-task')
+
 ]
